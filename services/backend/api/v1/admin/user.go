4cf7922030636a5f6e55a17e59d36cf13bbc099a
moba1 <moba2@protonmail.com>
2024-04-18 01:38:41 +0900

    パスワードのハッシュ化部分をログイン部分と共通化するために関数として抜き出した

--- services/backend/api/v1/admin/user.go
+++ services/backend/api/v1/admin/user.go
@@ -19,18 +19,27 @@ type User struct {
 	Password string
 }
 
+func hashPassword(p string) (string, error) {
+	s := sha512.Sum512([]byte(p))
+	hp, err := brcypt.GenerateFromPassword(s[:], 10)
+	if err != nil {
+		return nil, err
+	}
+	return hex.EncodeToString(hp), nil
+}
+
 func CreateUser(p *pgxpool.Pool) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		queries := db.New(p)
 
 		var u User
 		if err := c.ShouldBindJSON(&u); err != nil {
+			log.Println("[Info] reject user request with invalid structure")
 			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid user structure"})
 			return
 		}
 
-		s := sha512.Sum512([]byte(u.Password))
-		hp, err := bcrypt.GenerateFromPassword(s[:], 10)
+		hp, err := hashPassword(u.Password)
 		if err != nil {
 			log.Printf("[Error] cannot create user")
 			log.Println(err)
@@ -40,7 +49,7 @@ func CreateUser(p *pgxpool.Pool) gin.HandlerFunc {
 		cu, err := queries.CreateUser(c, db.CreateUserParams{
 			Name:     u.Name,
 			Email:    u.Email,
-			Password: hex.EncodeToString(hp),
+			Password: hp,
 		})
 		if err != nil {
 			log.Printf("[Error] cannot create user")
