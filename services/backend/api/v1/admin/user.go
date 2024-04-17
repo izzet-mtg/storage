e7f3109e43f2909942260b58f297e8ab4c88baa5
moba1 <moba2@protonmail.com>
2024-04-18 01:14:43 +0900

    パスワードをbcryptによってハッシュ化するようにした

--- services/backend/api/v1/admin/user.go
+++ services/backend/api/v1/admin/user.go
@@ -1,11 +1,14 @@
 package admin
 
 import (
+	"crypto/sha512"
+	"encoding/hex"
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"golang.org/x/crypto/bcrypt"
 
 	"github.com/izzet-mtg/storage/services/backend/db"
 )
@@ -26,10 +29,18 @@ func CreateUser(p *pgxpool.Pool) gin.HandlerFunc {
 			return
 		}
 
+		s := sha512.Sum512([]byte(u.Password))
+		hp, err := bcrypt.GenerateFromPassword(s[:], 16)
+		if err != nil {
+			log.Printf("[Error] cannot create user")
+			log.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
+			return
+		}
 		cu, err := queries.CreateUser(c, db.CreateUserParams{
 			Name:     u.Name,
 			Email:    u.Email,
-			Password: u.Password,
+			Password: hex.EncodeToString(hp),
 		})
 		if err != nil {
 			log.Printf("[Error] cannot create user")
